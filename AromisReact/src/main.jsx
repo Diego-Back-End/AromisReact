4e830656ca57af768d222f17e0dacd7f7b2713fb
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2025-10-15 18:27:05 -0300

    ajustes en el main

--- AromisReact/src/main.jsx
+++ AromisReact/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'  // <-- Importa BrowserRouter
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.jsx"; // 👈 Importante
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>  {/* <-- Envuelve tu App */}
-      <App />
+    <BrowserRouter>
+      <AuthProvider> {/* 👈 Envuelve toda tu app con el contexto */}
+        <App />
+      </AuthProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
