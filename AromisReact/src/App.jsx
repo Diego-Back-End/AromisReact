7c9ff19ee1050ff109630874a65b73b4daaf908d
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2025-10-15 18:25:30 -0300

    se modificaron las importanciones de la app.jsx

--- AromisReact/src/App.jsx
+++ AromisReact/src/App.jsx
@@ -1,4 +1,7 @@
+// App.jsx
 import { Routes, Route } from "react-router-dom";
+
+// Components
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import ProductList from "./components/ProductList";
@@ -7,25 +10,56 @@ import Footer from "./components/Footer";
 import Profile from "./components/Perfil";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import Catalogo from "./components/Catalogo"; 
+import Catalogo from "./components/Catalogo";
+import Carrito from "./components/Carrito";
+
+// Hooks
+import { useCart } from "./hooks/useCart";
+
 function App() {
+  const { cart, addToCart, removeFromCart, clearCart } = useCart();
+
   return (
     <>
       <Navbar />
       
-      {/* Aquí van las rutas */}
       <Routes>
-        <Route path="/" element={
-          <>
-            <Hero />
-            <ProductList />
-            <Filters />
-          </>
-        } />
-        <Route path="/perfil" element={<Profile />} />
+        {/* Home Route */}
+        <Route 
+          path="/" 
+          element={
+            <>
+              <Hero />
+              <ProductList addToCart={addToCart} />
+              <Filters />
+            </>
+          } 
+        />
+        
+        {/* Auth Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/catalogo" element={<Catalogo />} />
+        
+        {/* User Routes */}
+        <Route path="/perfil" element={<Profile />} />
+        
+        {/* Product Routes */}
+        <Route 
+          path="/catalogo" 
+          element={<Catalogo addToCart={addToCart} />} 
+        />
+        
+        {/* Cart Route */}
+        <Route 
+          path="/carrito" 
+          element={
+            <Carrito 
+              cart={cart} 
+              removeFromCart={removeFromCart} 
+              clearCart={clearCart} 
+            />
+          } 
+        />
       </Routes>
       
       <Footer />
