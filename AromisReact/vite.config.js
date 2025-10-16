6c7e54416d801d1108a92db499cd35ed5f6e8cd0
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2025-10-16 11:16:22 -0300

    configuracion del vite config para las pruebas unitarias

--- AromisReact/vite.config.js
+++ AromisReact/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+/**import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+})/**/
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.js",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+    },
+  },
+});
